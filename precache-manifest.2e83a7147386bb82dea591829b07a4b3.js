40632dbe87531711611e0ae1cbd8d3280e677125
Storey Sheldon <storey.e.sheldon@gmail.com>
2020-03-25 16:41:44 -0500

    Updates

---
+++ precache-manifest.2e83a7147386bb82dea591829b07a4b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3df83642a28b0ecb3e4596a0ec9b6d",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "008632a84a5694ca435d",
+    "url": "/React-Portfolio/static/css/main.790808fc.chunk.css"
+  },
+  {
+    "revision": "addcfa6604395167156c",
+    "url": "/React-Portfolio/static/js/2.15b8a26f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.15b8a26f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008632a84a5694ca435d",
+    "url": "/React-Portfolio/static/js/main.e1a97c08.chunk.js"
+  },
+  {
+    "revision": "b1cad2d636aa2549d2e5",
+    "url": "/React-Portfolio/static/js/runtime-main.bf13817d.js"
+  },
+  {
+    "revision": "278032a6bcd02dd23d756cf2fd3234b0",
+    "url": "/React-Portfolio/static/media/IMG_1383.278032a6.jpeg"
+  },
+  {
+    "revision": "52d0e140aa3f9b414f2ff054502585be",
+    "url": "/React-Portfolio/static/media/IMG_2.52d0e140.jpg"
+  }
+]);
